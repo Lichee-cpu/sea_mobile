40c47c94a23dc09611641c284ef1647662baef39
“lxiang” <1400256031@qq.com>
2022-07-27 13:48:37 +0800

    默认服务地址

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,8 @@ module.exports = {
   devServer: {
     proxy: {
       ["/"]: {
-        target: "http://localhost:8989",
+        // target: "http://localhost:8989",
+        target: "https://car-service.lichee.top",
         describe: "",
         secure: false,
         changeOrigin: true,
