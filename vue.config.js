250041e4c7b9381567c17bdf913ab606c3921850
“lxiang” <1400256031@qq.com>
2022-08-05 17:41:01 +0800

    1

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,13 @@
 module.exports = {
   devServer: {
-    host: "localhost",
-    open: true,
     proxy: {
-      "/": {
-        // target: "http://localhost:8989",
-        target: "https://car-service.lichee.top",
+      ["/"]: {
+        target: "http://localhost:8989",
+        // target: "https://car-service.lichee.top",
+        describe: "",
+        secure: false,
         changeOrigin: true,
+        logLevel: "debug",
       },
     },
   },
