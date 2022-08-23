d5dcbcbe49186f33339737067d3d78a4b73d9f54
home <1400256031@qq.com>
2022-08-23 21:33:44 +0800

    完成picker

--- src/views/project/picker/Picker.vue
+++ src/views/project/picker/Picker.vue
@@ -1,46 +1,26 @@
 <!--
  * @Author: home 1400256031@qq.com
- * @Date: 2022-08-21 21:00:57
+ * @Date: 2022-08-23 14:35:34
  * @LastEditors: home 1400256031@qq.com
- * @LastEditTime: 2022-08-22 00:38:18
+ * @LastEditTime: 2022-08-23 20:48:55
  * @FilePath: \sea_mobile\src\views\project\picker\Picker.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <template>
   <div>
-    <div class="_picker-box">
-      <div
-        class="_picker-content"
-        @touchstart="touchStart"
-        @touchmove="touchMove"
-        @touchend="touchEnd"
-      >
-        <ul
-          @click="clickul"
-          class="_picker-row-box"
-          :style="
-            'transform: translate3d(' +
-            state.translate +
-            ', 0px, 0px);transition: transform 300ms ease 0s;'
-          "
-        >
-          <li v-for="(i, o) in state.pickerData" :key="o" class="">
-            {{ i }}
-          </li>
-        </ul>
-      </div>
-    </div>
-    {{ state.pickerData[state.standIn] }}
+    <picker-2 :pickerData="state.pickerdata" @result="result" />
+    {{ state.result }}
   </div>
 </template>
-
 <script>
 import { reactive } from "@vue/reactivity";
-import { onMounted } from "@vue/runtime-core";
+import Picker2 from "./Picker2.vue";
+
 export default {
+  components: { Picker2 },
   setup() {
     const state = reactive({
-      pickerData: [
+      pickerdata: [
         "12-01",
         "12-02",
         "12-03",
@@ -62,170 +42,13 @@ export default {
         "12-19",
         "12-20",
       ],
-      min: 0,
-      max: 0,
-      translate: 0,
-      isPointerdown: false,
-      changeNum: 0, //改变的个数
-      standIn: 0, //指针
-      startPoint: 0, //起始点
-      movePoint: 0, //终点
-      changePoint: 0, //变化距离
-      guodu: 0, // 按下记录充当过渡
+      result: 0,
     });
-    const clickul = () => {
-      console.log("点击了ul");
-    };
-    const setTrans = () => {
-      const content = document.querySelector("._picker-content");
-      const li = document.querySelector("._picker-row-box");
-      const liWidth = li.querySelector("li").offsetWidth;
-      const rem = window.getComputedStyle(content, "after").width;
-
-      state.min = rem.replace(/[a-zA-Z]+/g, "");
-      state.max = liWidth * (state.pickerData.length - 1) - state.min;
-      state.translate = state.min + "px";
-      console.log("最大位置", state.max);
+    const result = (e) => {
+      state.result = e;
     };
 
-    const touchStart = (e) => {
-      if (e.pointerType === "mouse" && e.button !== 0) {
-        return;
-      }
-      state.isPointerdown = true;
-      state.guodu = Number(state.translate.replace(/[a-zA-Z]+/g, ""));
-      state.startPoint = e.touches[0].clientX;
-    };
-
-    const touchMove = (e) => {
-      if (state.isPointerdown) {
-        state.movePoint = e.targetTouches[0].clientX;
-        state.changePoint = state.movePoint - state.startPoint; //触摸移动距离
-        let newtrans = state.guodu + state.changePoint; //加上原始距离
-        console.log(
-          "移动距离+触摸距离",
-          state.changePoint,
-          state.guodu,
-          newtrans
-        );
-        if (Math.abs(state.changePoint) / 56 > 1) {
-          console.log("state.changePoint,", state.changePoint);
-          state.changeNum = Math.trunc(state.changePoint / 56); //取整
-          console.log("%c state.changeNum %s", "color: red; ", state.changeNum);
-          console.log("%c newtrans %s", "color: green; ", newtrans);
-          if (newtrans > state.min) {
-            state.translate = state.min + "px";
-            state.standIn = 0;
-          } else if (newtrans < -state.max) {
-            state.translate = -state.max + "px";
-          } else {
-            if (Math.abs(state.changeNum) > 0) {
-              console.log("大于0", Math.abs(state.changeNum));
-            }
-          }
-        }
-      }
-    };
-    const touchEnd = () => {
-      if (state.translate.replace(/[a-zA-Z]+/g, "") > state.min) {
-        state.translate = state.min + "px";
-        state.standIn = 0;
-      } else if (state.translate.replace(/[a-zA-Z]+/g, "") < -state.max) {
-        state.translate = -state.max + "px";
-        state.standIn = state.pickerData.length - 1;
-      } else {
-        state.translate =
-          state.changeNum * 56 +
-          Number(state.translate.replace(/[a-zA-Z]+/g, "")) +
-          "px";
-        state.standIn -= state.changeNum;
-      }
-
-      console.log(
-        "当前索引",
-        state.standIn,
-        state.translate.replace(/[a-zA-Z]+/g, "")
-      );
-      console.log(
-        "要设置为默认位置了",
-        state.translate.replace(/[a-zA-Z]+/g, ""),
-        state.min
-      );
-
-      state.startPoint = 0;
-      state.movePoint = 0;
-      state.changeNum = 0;
-      state.guodu = 0;
-      state.isPointerdown = false;
-    };
-
-    onMounted(() => {
-      setTrans();
-    });
-    return {
-      state,
-      touchStart,
-      touchMove,
-      touchEnd,
-      clickul,
-    };
+    return { state, result };
   },
 };
 </script>
-
-<style lang="less" scoped>
-._picker-box {
-  margin-top: 20px;
-  height: 50px;
-  display: flex;
-  align-items: center;
-  position: relative;
-  border: 1px solid #ebebeb;
-}
-._picker-content {
-  overflow: hidden;
-}
-._picker-row-box {
-  white-space: nowrap;
-  li {
-    display: inline-block;
-    width: 56px;
-    text-align: center;
-  }
-}
-
-._picker-content::before {
-  content: "";
-  position: absolute;
-  display: inline;
-  top: 0;
-  left: 0;
-  bottom: 0;
-  z-index: 1;
-  height: 50px;
-  width: calc(~"50% - 28px");
-  border-right: 1px solid #ebebeb;
-  background: linear-gradient(
-    to right,
-    rgba(255, 255, 255, 0.9),
-    rgba(255, 255, 255, 0.6)
-  );
-}
-._picker-content::after {
-  content: "";
-  position: absolute;
-  display: inline;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  z-index: 1;
-  height: 50px;
-  width: calc(~"50% - 28px");
-  border-left: 1px solid #ebebeb;
-  background: linear-gradient(
-    to left,
-    rgba(255, 255, 255, 0.9),
-    rgba(255, 255, 255, 0.6)
-  );
-}
-</style>
--- src/views/project/picker/Picker2.vue
+++ src/views/project/picker/Picker2.vue
@@ -1,36 +1,30 @@
-<!--
- * @Author: home 1400256031@qq.com
- * @Date: 2022-08-21 21:00:57
- * @LastEditors: home 1400256031@qq.com
- * @LastEditTime: 2022-08-22 00:38:18
- * @FilePath: \sea_mobile\src\views\project\picker\Picker.vue
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
--->
 <template>
   <div>
-    <div class="_picker-box">
+    <div class="picker-group">
       <div
-        class="_picker-content"
+        class="picker-row"
         @touchstart="touchStart"
         @touchmove="touchMove"
         @touchend="touchEnd"
       >
         <ul
-          @click="clickul"
-          class="_picker-row-box"
+          class="picker-content"
           :style="
             'transform: translate3d(' +
-            state.translate +
-            ', 0px, 0px);transition: transform 300ms ease 0s;'
+            option.translateX +
+            'px, 0px, 0px);transition: transform 300ms ease 0s;display: flex;'
           "
         >
-          <li v-for="(i, o) in state.pickerData" :key="o" class="">
+          <li
+            v-for="(i, o) in state.pickerData"
+            :key="o"
+            :class="option.activeItem == o ? 'active' : ''"
+          >
             {{ i }}
           </li>
         </ul>
       </div>
     </div>
-    {{ state.pickerData[state.standIn] }}
   </div>
 </template>
 
@@ -38,166 +32,159 @@
 import { reactive } from "@vue/reactivity";
 import { onMounted } from "@vue/runtime-core";
 export default {
-  setup() {
+  props: {
+    pickerData: {
+      typeof: Array,
+      default: () => {
+        return [];
+      },
+    },
+  },
+  setup(props, { emit }) {
     const state = reactive({
-      pickerData: [
-        "12-01",
-        "12-02",
-        "12-03",
-        "12-04",
-        "12-05",
-        "12-06",
-        "12-07",
-        "12-08",
-        "12-09",
-        "12-10",
-        "12-11",
-        "12-12",
-        "12-13",
-        "12-14",
-        "12-15",
-        "12-16",
-        "12-17",
-        "12-18",
-        "12-19",
-        "12-20",
-      ],
-      min: 0,
-      max: 0,
-      translate: 0,
-      isPointerdown: false,
-      changeNum: 0, //改变的个数
-      standIn: 0, //指针
-      startPoint: 0, //起始点
-      movePoint: 0, //终点
-      changePoint: 0, //变化距离
-      guodu: 0, // 按下记录充当过渡
+      pickerData: props.pickerData,
     });
-    const clickul = () => {
-      console.log("点击了ul");
+    const option = reactive({
+      isPointerdown: false,
+      ul: null,
+      pickerRow: null,
+      itemWidth: 56, // 列表项宽度
+      pseudoWidth: 0, //伪元素宽度
+      maxX: 0, //初始位置也是最大的位置
+      minX: 0, //最小的位置
+      lastX: 0,
+      diffX: 0,
+      translateX: 0, // 当前位置
+      friction: 0.95, // 摩擦系数
+      distanceX: 0, // 滑动距离
+      activeItem: 0,
+      result: state.pickerData[0],
+    });
+    /**
+     * @description: 初始化
+     * @return {*}
+     */
+    const render = () => {
+      option.ul = document.querySelector(".picker-content");
+      option.pickerRow = document.querySelector(".picker-row");
+      option.itemWidth = option.ul.querySelector("li").offsetWidth; // 列表项宽度
+      option.pseudoWidth = window.getComputedStyle(
+        option.pickerRow,
+        "after"
+      ).width; //伪元素宽度
+      option.maxX = option.pseudoWidth.replace(/[a-zA-Z]+/g, ""); //初始位置也是最大的位置
+      option.minX =
+        option.itemWidth * (state.pickerData.length - 1) - option.maxX; //最小的位置
+      option.translateX = option.maxX;
+      emit("result", option.result);
     };
-    const setTrans = () => {
-      const content = document.querySelector("._picker-content");
-      const li = document.querySelector("._picker-row-box");
-      const liWidth = li.querySelector("li").offsetWidth;
-      const rem = window.getComputedStyle(content, "after").width;
 
-      state.min = rem.replace(/[a-zA-Z]+/g, "");
-      state.max = liWidth * (state.pickerData.length - 1) - state.min;
-      state.translate = state.min + "px";
-      console.log("最大位置", state.max);
-    };
-
+    /**
+     * @description: 点击
+     * @param {*} e
+     * @return {*}
+     */
     const touchStart = (e) => {
-      if (e.pointerType === "mouse" && e.button !== 0) {
-        return;
-      }
-      state.isPointerdown = true;
-      state.guodu = Number(state.translate.replace(/[a-zA-Z]+/g, ""));
-      state.startPoint = e.touches[0].clientX;
+      option.isPointerdown = true;
+      option.lastX = e.touches[0].clientX;
+      option.diffX = 0;
+      option.distanceX = 0;
+      getTransform();
     };
-
+    /**
+     * @description: 移动
+     * @param {*} e
+     * @return {*}
+     */
     const touchMove = (e) => {
-      if (state.isPointerdown) {
-        state.movePoint = e.targetTouches[0].clientX;
-        state.changePoint = state.movePoint - state.startPoint; //触摸移动距离
-        let newtrans = state.guodu + state.changePoint; //加上原始距离
-        console.log(
-          "移动距离+触摸距离",
-          state.changePoint,
-          state.guodu,
-          newtrans
-        );
-
-        // if (Math.abs(state.changePoint) / 56 > 1) {
-        console.log("state.changePoint,", state.changePoint);
-        state.changeNum = Math.trunc(state.changePoint / 56); //取整
-        console.log("%c state.changeNum %s", "color: red; ", state.changeNum);
-        console.log("%c newtrans %s", "color: green; ", newtrans);
-        if (newtrans > state.min) {
-          state.translate = state.min + "px";
-          state.standIn = 0;
-        } else if (newtrans < -state.max) {
-          state.translate = -state.max + "px";
-        } else {
-          state.standIn -= state.changeNum;
-          console.log("%c索引%s", "color: yellow; ", state.standIn);
-          if (Math.abs(state.changeNum) > 0) {
-            console.log("大于0", Math.abs(state.changeNum));
-          }
-        }
-        // }
+      if (option.isPointerdown) {
+        option.diffX = e.touches[0].clientX - option.lastX;
+        option.translateX += option.diffX;
+        option.lastX = e.touches[0].clientX;
       }
     };
+    /**
+     * @description: 结束
+     * @param {*} e
+     * @return {*}
+     */
     const touchEnd = () => {
-      if (state.translate.replace(/[a-zA-Z]+/g, "") > state.min) {
-        state.translate = state.min + "px";
-        state.standIn = 0;
-      } else if (state.translate.replace(/[a-zA-Z]+/g, "") < -state.max) {
-        state.translate = -state.max + "px";
-        state.standIn = state.pickerData.length - 1;
+      if (option.isPointerdown) {
+        option.isPointerdown = false;
+        getTranslateX();
+
+        // 滑动距离与时长成正比且最短时长为300ms
+        const duration = Math.max(Math.abs(option.distanceX) * 1.5, 300);
+        option.ul.style.transition = "transform " + duration + "ms ease";
+      }
+    };
+
+    /**
+     * @description: 设置位置及返回数据
+     * @return {*}
+     */
+    const getTranslateX = () => {
+      let speed = option.diffX;
+      while (Math.abs(speed) > 1) {
+        speed *= option.friction;
+        option.distanceX += speed;
+      }
+      // 边界判断
+      let y = option.translateX + option.distanceX;
+      if (y > option.maxX) {
+        option.translateX = option.maxX;
+        option.distanceX = option.maxX - option.translateX;
+      } else if (y < -option.minX) {
+        option.translateX = -option.minX;
+        option.distanceX = option.minX - option.translateX;
       } else {
-        state.translate =
-          state.changeNum * 56 +
-          Number(state.translate.replace(/[a-zA-Z]+/g, "")) +
-          "px";
-        state.standIn -= state.changeNum;
+        option.translateX = y;
       }
 
-      console.log(
-        "当前索引",
-        state.standIn,
-        state.translate.replace(/[a-zA-Z]+/g, "")
-      );
-      console.log(
-        "要设置为默认位置了",
-        state.translate.replace(/[a-zA-Z]+/g, ""),
-        state.min
-      );
+      // 计算停止位置使其为itemWidth的整数倍
+      let i = Math.round((option.translateX - option.maxX) / option.itemWidth);
+      option.translateX = Number(option.maxX) + Number(i * option.itemWidth);
+      option.activeItem = Math.abs(i);
+      option.result = state.pickerData[Math.abs(i)];
 
-      state.startPoint = 0;
-      state.movePoint = 0;
-      state.changeNum = 0;
-      state.guodu = 0;
-      state.isPointerdown = false;
+      emit("result", option.result);
+    };
+    /**
+     * @description: 设点击时初始位置
+     * @return {*}
+     */
+    const getTransform = () => {
+      const transform = window
+        .getComputedStyle(option.ul)
+        .getPropertyValue("transform");
+      option.translateX = parseFloat(transform.split(",")[4]);
+      option.ul.style.transition = "none";
     };
 
     onMounted(() => {
-      setTrans();
+      render();
     });
-    return {
-      state,
-      touchStart,
-      touchMove,
-      touchEnd,
-      clickul,
-    };
+
+    return { state, option, touchStart, touchMove, touchEnd };
   },
 };
 </script>
 
 <style lang="less" scoped>
-._picker-box {
-  margin-top: 20px;
-  height: 50px;
+.picker-group {
   display: flex;
-  align-items: center;
-  position: relative;
-  border: 1px solid #ebebeb;
-}
-._picker-content {
-  overflow: hidden;
-}
-._picker-row-box {
-  white-space: nowrap;
-  li {
-    display: inline-block;
-    width: 56px;
-    text-align: center;
-  }
 }
 
-._picker-content::before {
+.picker-row {
+  position: relative;
+  flex: 1;
+  margin: auto 0;
+  overflow: hidden;
+  touch-action: none;
+  display: flex;
+}
+
+.picker-row::before {
   content: "";
   position: absolute;
   display: inline;
@@ -205,30 +192,46 @@ export default {
   left: 0;
   bottom: 0;
   z-index: 1;
-  height: 50px;
   width: calc(~"50% - 28px");
   border-right: 1px solid #ebebeb;
   background: linear-gradient(
-    to right,
+    to bottom,
+    rgba(255, 255, 255, 0.6),
+    rgba(255, 255, 255, 0.9)
+  );
+}
+
+.picker-row::after {
+  content: "";
+  position: absolute;
+  display: inline;
+  bottom: 0;
+  right: 0;
+  top: 0;
+  z-index: 1;
+  width: calc(~"50% - 28px");
+  border-left: 1px solid #ebebeb;
+  background: linear-gradient(
+    to bottom,
     rgba(255, 255, 255, 0.9),
     rgba(255, 255, 255, 0.6)
   );
 }
-._picker-content::after {
-  content: "";
-  position: absolute;
-  display: inline;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  z-index: 1;
-  height: 50px;
-  width: calc(~"50% - 28px");
-  border-left: 1px solid #ebebeb;
-  background: linear-gradient(
-    to left,
-    rgba(255, 255, 255, 0.9),
-    rgba(255, 255, 255, 0.6)
-  );
+
+li {
+  list-style: none;
+  font-size: 14px;
+  width: 56px;
+  line-height: 20px;
+  text-align: center;
+}
+.active {
+  font-size: 16px;
+  font-weight: 700;
+}
+
+.btn-sure {
+  display: block;
+  margin: 15px auto 0;
 }
 </style>
