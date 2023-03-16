48e9c9e5068f95fea194739da556015f591690a4
xliang <xliang@amarsoft.com>
2023-03-16 14:03:11 +0800

    代理

--- vue.config.js
+++ vue.config.js
@@ -15,6 +15,9 @@ module.exports = {
         secure: false, // 如果是https接口，需要配置这个参数
         changeOrigin: true, // 如果接口跨域，需要进行这个参数配置
         logLevel: "debug", // 日志级别
+        pathRewrite: {
+          "^/cgi-bin": "/cgi-bin",
+        }, // 重写路径 
       },
       "/a": {
         // target: "http://localhost:8989",
