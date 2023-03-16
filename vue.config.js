df596776a43ccd6ab133fbe66c750326da2c8f0f
xliang <xliang@amarsoft.com>
2023-03-16 09:02:54 +0800

    1

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@
  * @Author: lxiang
  * @Date: 2022-08-05 17:14:24
  * @LastEditors: lxiang
- * @LastEditTime: 2023-03-16 08:36:20
+ * @LastEditTime: 2023-03-16 09:02:37
  * @description: Modify here please
  * @FilePath: \sea_mobile\vue.config.js
  */
@@ -12,11 +12,11 @@ module.exports = {
       "/cgi-bin": {
         target: "https://qyapi.weixin.qq.com",
         describe: "", // 代理描述
-        secure: true, // 如果是https接口，需要配置这个参数
+        secure: false, // 如果是https接口，需要配置这个参数
         changeOrigin: true, // 如果接口跨域，需要进行这个参数配置
         logLevel: "debug", // 日志级别
       },
-      "/": {
+      "/a": {
         // target: "http://localhost:8989",
         target: "https://car-service.lichee.top",
         describe: "",
