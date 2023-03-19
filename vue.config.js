8907872bd1bf6b464ed5ab1e3677ae7d3f45bb9b
xliang <xliang@amarsoft.com>
2023-03-19 13:06:14 +0800

    代理

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,8 @@
-/*
- * @Author: lxiang
- * @Date: 2022-08-05 17:14:24
- * @LastEditors: lxiang
- * @LastEditTime: 2023-03-19 11:01:30
- * @description: Modify here please
- * @FilePath: \sea_mobile\vue.config.js
- */
+
 module.exports = {
   devServer: {
     proxy: {
-      "/a": {
+      "/": {
         // target: "http://localhost:8989",
         target: "https://car-service.lichee.top",
         describe: "",
