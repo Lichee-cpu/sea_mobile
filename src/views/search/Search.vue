a4f3604e41d6315c2bba6a314861b469ff646b52
xliang <xliang@amarsoft.com>
2023-03-19 21:08:53 +0800

    二次定位

--- src/views/search/Search.vue
+++ src/views/search/Search.vue
@@ -2,7 +2,7 @@
  * @Author: lxiang
  * @Date: 2022-05-31 10:10:24
  * @LastEditors: lxiang
- * @LastEditTime: 2023-03-19 19:45:26
+ * @LastEditTime: 2023-03-19 21:08:27
  * @description: Modify here please
  * @FilePath: \sea_mobile\src\views\search\Search.vue
 -->
@@ -50,9 +50,32 @@ export default {
               location.value = `纬度：${latitude}，经度：${longitude}，精度：${accuracy}`;
               Toast.success("定位已开启");
             },
-            function () {
-              active.value = false;
-              Toast.fail("定位失败");
+            function (error) {
+              // 获取设备位置信息失败
+              if (error.code == error.PERMISSION_DENIED) {
+                // 用户拒绝了定位请求，需要再次请求定位权限
+                navigator.geolocation.getCurrentPosition(
+                  function (position) {
+                    // 获取到设备的位置信息
+                    var latitude = position.coords.latitude; // 纬度
+                    var longitude = position.coords.longitude; // 经度
+                    var accuracy = position.coords.accuracy; // 精度
+                    localStorage.setItem(
+                      "location",
+                      JSON.stringify({ latitude, longitude, accuracy })
+                    );
+                    location.value = `纬度：${latitude}，经度：${longitude}，精度：${accuracy}`;
+                    Toast.success("定位已开启");
+                  },
+                  function () {
+                    active.value = false;
+                    Toast.fail("二次定位失败");
+                  }
+                );
+              } else {
+                active.value = false;
+                Toast.fail("定位失败");
+              }
             }
           );
         } else {
