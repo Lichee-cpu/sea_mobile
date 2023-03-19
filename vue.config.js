aa8df6cd1946479109aece9cf33be10f15d9fb26
xliang <xliang@amarsoft.com>
2023-03-19 14:40:00 +0800

    更改为推荐

--- vue.config.js
+++ vue.config.js
@@ -2,11 +2,8 @@ module.exports = {
   devServer: {
     proxy: {
       "/": {
-        // target: "http://localhost:8989",
         target: "https://car-service.lichee.top",
-        secure: false,
         changeOrigin: true,
-        logLevel: "debug",
         pathRewrite: {
           "^/": "/",
         },
