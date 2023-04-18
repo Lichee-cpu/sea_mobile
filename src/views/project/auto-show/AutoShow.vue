0365ce280e3a27c8c6bad7970482dd9d7eec8fe2
xliang <xliang@amarsoft.com>
2023-04-18 11:22:07 +0800

    优化车展

--- src/views/project/auto-show/AutoShow.vue
+++ src/views/project/auto-show/AutoShow.vue
@@ -2,7 +2,7 @@
  * @Author: home 1400256031@qq.com
  * @Date: 2022-08-13 09:52:55
  * @LastEditors: lxiang
- * @LastEditTime: 2023-04-18 11:10:41
+ * @LastEditTime: 2023-04-18 11:22:01
  * @FilePath: \sea_mobile\src\views\project\auto-show\AutoShow.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -28,7 +28,6 @@
           />
         </div>
       </div>
-      <input type="range" :max="max" :min="min" v-model="num" />
     </div>
   </div>
 </template>
@@ -48,21 +47,21 @@ export default {
       dragWidth: 0, // 拖动元素容器宽度
       startNum: 0, // 初始值
       start: 0,
-      end: 0,
     });
 
     const touchStart = (e) => {
-      // 设定初始位置
-      option.start = e.touches[0].clientX;
-      option.startNum = num.value;
+      option.start = e.touches[0].clientX; // 记录初始位置
+      option.startNum = num.value; // 记录初始值
     };
     const touchMove = (e) => {
       const diffX = e.touches[0].clientX - option.start;
       // 计算距离加多少张图片
-      const one = option.dragWidth / 2; //转一圈的距离
-      const page = one / max.value; // 每张图片的距离
-      const diffPage = Math.floor(diffX / page); // 转了几张图片
-      let newNum = option.startNum + diffPage;
+      const one = option.dragWidth / 2; //多少占比能够转完一圈
+      const page = one / max.value; //移动多少距离转一张图片
+      const diffPage = Math.floor(diffX / page); //转了几张图片
+      let newNum = option.startNum - diffPage; // 新的值
+
+      // 边界判断
       if (newNum > max.value) {
         option.startNum = min.value;
         newNum = newNum - max.value;
@@ -70,13 +69,13 @@ export default {
         option.startNum = max.value;
         newNum = newNum + max.value;
       }
-
       num.value = newNum;
     };
 
     onMounted(() => {
       option.dragWidth = drag.value.offsetWidth;
     });
+
     return {
       drag,
       num,
@@ -92,7 +91,6 @@ export default {
 <style lang="less" scoped>
 .autoShowBox {
   padding-top: var(--nav-bar-height);
-  height: 50vh;
   position: relative;
   img {
     width: 100%;
