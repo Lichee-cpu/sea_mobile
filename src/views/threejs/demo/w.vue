4f8b4efca385fe37f51c7981e9dd7ce654b14334
xliang <xliang@amarsoft.com>
2023-04-28 16:30:23 +0800

    陀螺仪

--- src/views/threejs/demo/w.vue
+++ src/views/threejs/demo/w.vue
@@ -2,9 +2,9 @@
  * @Author: lxiang
  * @Date: 2023-04-23 08:47:02
  * @LastEditors: lxiang
- * @LastEditTime: 2023-04-28 14:41:24
+ * @LastEditTime: 2023-04-28 16:29:14
  * @description: VR看房
- * @FilePath: \sea_mobile\src\views\threejs\demo\v.vue
+ * @FilePath: \sea_mobile\src\views\threejs\demo\w.vue
 -->
 <template>
   <div class="info">
@@ -17,12 +17,28 @@
 import Header from "@/components/header/Header.vue";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader";
+// import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader";
 import { ref, onMounted, onBeforeUnmount } from "vue";
 import { useRoute } from "vue-router";
 
 const route = useRoute();
 const title = route?.query?.title;
+
+const handleDeviceOrientation = (event) => {
+  // 获取陀螺仪数据
+  console.log("陀螺仪", event);
+  const alpha = event.alpha;
+  const beta = event.beta;
+  const gamma = event.gamma;
+
+  // 将陀螺仪数据应用于立方体的旋转
+  cube.rotation.x = (beta * Math.PI) / 180; // 俯仰角
+  cube.rotation.y = (gamma * Math.PI) / 180; // 方位角
+  cube.rotation.z = (alpha * Math.PI) / 180; // 滚动角
+
+  // 渲染场景
+  renderer.render(scene, camera);
+};
 
 // 初始化场景
 const scene = new THREE.Scene();
@@ -41,42 +57,42 @@ const render = () => {
 };
 
 // 添加立方体
-// const geometry = new THREE.BoxGeometry(10, 10, 10);
-// // const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-// // const cube = new THREE.Mesh(geometry, material);
-// // scene.add(cube);
-
-// // 4_b,
-// var arr = ["4_l", "4_r", "4_u", "4_d", "4_b", "4_f"];
-// var boxMaterials = [];
-
-// arr.forEach((item) => {
-//   // 纹理加载
-//   let texture = new THREE.TextureLoader().load(
-//     require(`@/assets/textures/living/${item}.jpg`)
-//   );
-//   // 创建材质
-//   if (item === "4_u" || item === "4_d") {
-//     texture.rotation = Math.PI; // 旋转180度
-//     texture.center = new THREE.Vector2(0.5, 0.5); // 设置纹理中心点
-//     boxMaterials.push(new THREE.MeshBasicMaterial({ map: texture }));
-//   } else {
-//     boxMaterials.push(new THREE.MeshBasicMaterial({ map: texture }));
-//   }
-// });
-// const cube = new THREE.Mesh(geometry, boxMaterials);
-// cube.geometry.scale(1, 1, -1); // 翻转几何体反过来,否则内部看不到
+const geometry = new THREE.BoxGeometry(10, 10, 10);
+// const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+// const cube = new THREE.Mesh(geometry, material);
 // scene.add(cube);
 
-// 添加球
-const geometry = new THREE.SphereGeometry(5, 32, 32);
-const loader = new RGBELoader();
-loader.load("./threejsModules/Living.hdr", (texture) => {
-  const material = new THREE.MeshBasicMaterial({ map: texture });
-  const sphere = new THREE.Mesh(geometry, material);
-  sphere.geometry.scale(1, 1, -1);
-  scene.add(sphere);
+// 4_b,
+var arr = ["4_l", "4_r", "4_u", "4_d", "4_b", "4_f"];
+var boxMaterials = [];
+
+arr.forEach((item) => {
+  // 纹理加载
+  let texture = new THREE.TextureLoader().load(
+    require(`@/assets/textures/living/${item}.jpg`)
+  );
+  // 创建材质
+  if (item === "4_u" || item === "4_d") {
+    texture.rotation = Math.PI; // 旋转180度
+    texture.center = new THREE.Vector2(0.5, 0.5); // 设置纹理中心点
+    boxMaterials.push(new THREE.MeshBasicMaterial({ map: texture }));
+  } else {
+    boxMaterials.push(new THREE.MeshBasicMaterial({ map: texture }));
+  }
 });
+const cube = new THREE.Mesh(geometry, boxMaterials);
+cube.geometry.scale(1, 1, -1); // 翻转几何体反过来,否则内部看不到
+scene.add(cube);
+
+// 添加球
+// const geometry = new THREE.SphereGeometry(5, 32, 32);
+// const loader = new RGBELoader();
+// loader.load("./threejsModules/Living.hdr", (texture) => {
+//   const material = new THREE.MeshBasicMaterial({ map: texture });
+//   const sphere = new THREE.Mesh(geometry, material);
+//   sphere.geometry.scale(1, 1, -1);
+//   scene.add(sphere);
+// });
 
 // 挂载完毕之后获取dom
 onMounted(() => {
@@ -99,9 +115,28 @@ onMounted(() => {
     // 更新相机投影矩阵
     camera.updateProjectionMatrix();
   });
+
+  window.addEventListener("dblclick", () => {
+    // 全屏和退出全屏
+    if (document.fullscreenElement) {
+      document.exitFullscreen(); // 退出全屏
+    } else {
+      box.value.requestFullscreen(); // 全屏
+    }
+  });
+  if (window.DeviceOrientationEvent) {
+    console.log("支持陀螺仪");
+    // 添加陀螺仪事件监听器
+    window.addEventListener("deviceorientation", handleDeviceOrientation);
+  } else {
+    console.log("不支持陀螺仪");
+  }
 });
 onBeforeUnmount(() => {
   window.removeEventListener("resize", () => {});
+  window.removeEventListener("dblclick", () => {});
+  if (window.DeviceOrientationEvent)
+    window.removeEventListener("deviceorientation", handleDeviceOrientation);
 });
 </script>
 <style lang="less" scoped>
