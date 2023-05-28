d45c62a1727debe34d4787be399f71136b9b2148
xliang <xliang@amarsoft.com>
2023-05-28 20:23:10 +0800

    没有检测到钱包就返回

--- src/views/web3/web3.js
+++ src/views/web3/web3.js
@@ -1,6 +1,9 @@
 import Web3 from "web3";
+import { Toast } from "vant";
+import { useRouter } from "vue-router";
 
 let web3;
+const router = useRouter();
 
 if (window.ethereum) {
   web3 = new Web3(window.ethereum);
@@ -13,6 +16,8 @@ if (window.ethereum) {
 } else if (window.web3) {
   web3 = new Web3(window.web3.currentProvider);
 } else {
+  Toast.fail("请安装以太坊钱包");
+  router.go(-1); // 返回上一页
   console.error("没有检测到以太坊钱包");
 }
 
